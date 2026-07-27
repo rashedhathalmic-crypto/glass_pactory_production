74ae602feecf7f377e9a9a2b4fbdb3f8f6a8f26a
rashedhathalmic-crypto <rashedhathalmic@gmail.com>
2026-07-27 09:56:11 +0300

    Implement native Excel-parity NC generator

--- cnc_nc_generator/__init__.py
+++ cnc_nc_generator/__init__.py
@@ -1,5 +1,6 @@
-"""Workbook-backed SKG1625 / Shanlong L68 NC export tools."""
+"""Native SKG1625 / Shanlong L68 NC generation tools."""
 
+from .native_nc import NCParameters, SUPPORTED_PROFILES, generate_nc
 from .workbook_engine import WorkbookCell, WorkbookEngine, WorksheetModel
 
-__all__ = ["WorkbookCell", "WorkbookEngine", "WorksheetModel"]
+__all__ = ["NCParameters", "SUPPORTED_PROFILES", "generate_nc", "WorkbookCell", "WorkbookEngine", "WorksheetModel"]
