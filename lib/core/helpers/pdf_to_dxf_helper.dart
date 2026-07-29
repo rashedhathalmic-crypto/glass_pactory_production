8a127000f6aede16d7d453faf2967ecfb4795708
rashedhathalmic-crypto <rashedhathalmic@gmail.com>
2026-07-29 08:16:00 +0300

    Remove manual dimensions from automatic PDF conversion

--- lib/core/helpers/pdf_to_dxf_helper.dart
+++ lib/core/helpers/pdf_to_dxf_helper.dart
@@ -3,14 +3,5 @@ import 'dart:typed_data';
 import 'pdf_to_dxf_helper_stub.dart'
     if (dart.library.html) 'pdf_to_dxf_helper_web.dart' as impl;
 
-Future<String> convertPdfToDxf({
-  required Uint8List bytes,
-  required double lengthMm,
-  required double widthMm,
-  required double angleDeg,
-}) => impl.convertPdfToDxf(
-  bytes: bytes,
-  lengthMm: lengthMm,
-  widthMm: widthMm,
-  angleDeg: angleDeg,
-);
+Future<String> convertPdfToDxf({required Uint8List bytes}) =>
+    impl.convertPdfToDxf(bytes: bytes);
