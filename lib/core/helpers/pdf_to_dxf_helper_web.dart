32e376bf4b5ee8a800e13ec8c2a5d728e76c40fd
rashedhathalmic-crypto <rashedhathalmic@gmail.com>
2026-07-29 09:46:06 +0300

    Fix Flutter callback handling for PDF conversion

--- lib/core/helpers/pdf_to_dxf_helper_web.dart
+++ lib/core/helpers/pdf_to_dxf_helper_web.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: avoid_web_libraries_in_flutter, deprecated_member_use
 
+import 'dart:async';
 import 'dart:js' as js;
-import 'dart:js_util' as js_util;
 import 'dart:typed_data';
 
-Future<String> convertPdfToDxf({required Uint8List bytes}) async {
-  final result = js.context.callMethod('pdfToDxf2d', [bytes]);
-  return (await js_util.promiseToFuture<Object>(result)).toString();
+Future<String> convertPdfToDxf({required Uint8List bytes}) {
+  final completer = Completer<String>();
+  js.context.callMethod('pdfToDxf2dCallback', [
+    bytes,
+    js.allowInterop((Object result) => completer.complete(result.toString())),
+    js.allowInterop((Object error) => completer.completeError(error.toString())),
+  ]);
+  return completer.future;
 }
