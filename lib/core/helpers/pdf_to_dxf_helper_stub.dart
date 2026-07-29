346fbad97be0eaa8783bd75b51729b482ac279ea
rashedhathalmic-crypto <rashedhathalmic@gmail.com>
2026-07-29 08:14:35 +0300

    Add non-web PDF converter stub

---
+++ lib/core/helpers/pdf_to_dxf_helper_stub.dart
@@ -0,0 +1,10 @@
+import 'dart:typed_data';
+
+Future<String> convertPdfToDxf({
+  required Uint8List bytes,
+  required double lengthMm,
+  required double widthMm,
+  required double angleDeg,
+}) {
+  throw UnsupportedError('PDF to DXF conversion is available in the web app.');
+}
