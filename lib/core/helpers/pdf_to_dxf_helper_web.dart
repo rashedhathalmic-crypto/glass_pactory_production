1d9b068408db16d907d378b712b6230fb9029cd5
rashedhathalmic-crypto <rashedhathalmic@gmail.com>
2026-07-29 08:14:39 +0300

    Connect Flutter web to PDF vector converter

---
+++ lib/core/helpers/pdf_to_dxf_helper_web.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: avoid_web_libraries_in_flutter, deprecated_member_use
+
+import 'dart:js' as js;
+import 'dart:js_util' as js_util;
+import 'dart:typed_data';
+
+Future<String> convertPdfToDxf({
+  required Uint8List bytes,
+  required double lengthMm,
+  required double widthMm,
+  required double angleDeg,
+}) async {
+  final result = js.context.callMethod<Object>('pdfToDxf2d', [
+    bytes,
+    lengthMm,
+    widthMm,
+    angleDeg,
+  ]);
+  return (await js_util.promiseToFuture<Object>(result)).toString();
+}
