9552ccc5bbe33cffd9159a364721907064ead4c2
rashedhathalmic-crypto <rashedhathalmic@gmail.com>
2026-07-30 07:01:49 +0300

    Add verified PDF profile selection and dimension review

--- lib/core/helpers/pdf_to_dxf_helper.dart
+++ lib/core/helpers/pdf_to_dxf_helper.dart
@@ -1,7 +1,11 @@
 import 'dart:typed_data';
 
+import 'pdf_profile_analysis.dart';
 import 'pdf_to_dxf_helper_stub.dart'
     if (dart.library.html) 'pdf_to_dxf_helper_web.dart' as impl;
 
 Future<String> convertPdfToDxf({required Uint8List bytes}) =>
     impl.convertPdfToDxf(bytes: bytes);
+
+Future<PdfProfileAnalysis> analyzePdfProfiles({required Uint8List bytes}) =>
+    impl.analyzePdfProfiles(bytes: bytes);
