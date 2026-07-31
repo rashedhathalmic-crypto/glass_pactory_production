ed363064f6523c546f29a0eb6eea35c09c935211
rashedhathalmic-crypto <rashedhathalmic@gmail.com>
2026-07-31 18:36:31 +0300

    Use Dart 3.12 JavaScript Promise interop

--- lib/core/helpers/pdf_to_dxf_helper_web.dart
+++ lib/core/helpers/pdf_to_dxf_helper_web.dart
@@ -1,72 +1,53 @@
-// ignore_for_file: avoid_web_libraries_in_flutter, deprecated_member_use
-
-import 'dart:async';
 import 'dart:convert';
-import 'dart:js' as js;
+import 'dart:js_interop';
 import 'dart:typed_data';
 
 import 'pdf_profile_analysis.dart';
 
-Future<String> convertPdfToDxf({required Uint8List bytes}) {
-  final completer = Completer<String>();
-  js.context.callMethod('pdfToDxf2dCallback', [
-    bytes,
-    js.allowInterop((Object result) => completer.complete(result.toString())),
-    js.allowInterop((Object error) => completer.completeError(error.toString())),
-  ]);
-  return completer.future;
+@JS('pdfToDxf2d')
+external JSPromise<JSString> _pdfToDxf2d(JSUint8Array bytes);
+
+@JS('pdfAnalyze2d')
+external JSPromise<JSString> _pdfAnalyze2d(JSUint8Array bytes);
+
+@JS('clipboardImageAnalyze2d')
+external JSPromise<JSString> _clipboardImageAnalyze2d();
+
+@JS('drawingImageAnalyze2d')
+external JSPromise<JSString> _drawingImageAnalyze2d(
+  JSUint8Array bytes,
+  JSString contentType,
+);
+
+Future<String> convertPdfToDxf({required Uint8List bytes}) async {
+  final result = await _pdfToDxf2d(bytes.toJS).toDart;
+  return result.toDart;
 }
 
-Future<PdfProfileAnalysis> analyzePdfProfiles({required Uint8List bytes}) {
-  final completer = Completer<PdfProfileAnalysis>();
-  js.context.callMethod('pdfAnalyze2dCallback', [
-    bytes,
-    js.allowInterop((Object result) {
-      try {
-        final json = jsonDecode(result.toString()) as Map<String, dynamic>;
-        completer.complete(PdfProfileAnalysis.fromJson(json));
-      } on Object catch (error, stackTrace) {
-        completer.completeError(error, stackTrace);
-      }
-    }),
-    js.allowInterop((Object error) => completer.completeError(error.toString())),
-  ]);
-  return completer.future;
+Future<PdfProfileAnalysis> analyzePdfProfiles({
+  required Uint8List bytes,
+}) async {
+  final result = await _pdfAnalyze2d(bytes.toJS).toDart;
+  return _decodeAnalysis(result.toDart);
 }
 
-Future<PdfProfileAnalysis> analyzeClipboardDrawing() {
-  final completer = Completer<PdfProfileAnalysis>();
-  js.context.callMethod('clipboardImageAnalyze2dCallback', [
-    js.allowInterop((Object result) {
-      try {
-        final json = jsonDecode(result.toString()) as Map<String, dynamic>;
-        completer.complete(PdfProfileAnalysis.fromJson(json));
-      } on Object catch (error, stackTrace) {
-        completer.completeError(error, stackTrace);
-      }
-    }),
-    js.allowInterop((Object error) => completer.completeError(error.toString())),
-  ]);
-  return completer.future;
+Future<PdfProfileAnalysis> analyzeClipboardDrawing() async {
+  final result = await _clipboardImageAnalyze2d().toDart;
+  return _decodeAnalysis(result.toDart);
 }
 
 Future<PdfProfileAnalysis> analyzeDrawingImage({
   required Uint8List bytes,
   required String contentType,
-}) {
-  final completer = Completer<PdfProfileAnalysis>();
-  js.context.callMethod('drawingImageAnalyze2dCallback', [
-    bytes,
-    contentType,
-    js.allowInterop((Object result) {
-      try {
-        final json = jsonDecode(result.toString()) as Map<String, dynamic>;
-        completer.complete(PdfProfileAnalysis.fromJson(json));
-      } on Object catch (error, stackTrace) {
-        completer.completeError(error, stackTrace);
-      }
-    }),
-    js.allowInterop((Object error) => completer.completeError(error.toString())),
-  ]);
-  return completer.future;
+}) async {
+  final result = await _drawingImageAnalyze2d(
+    bytes.toJS,
+    contentType.toJS,
+  ).toDart;
+  return _decodeAnalysis(result.toDart);
+}
+
+PdfProfileAnalysis _decodeAnalysis(String source) {
+  final json = jsonDecode(source) as Map<String, dynamic>;
+  return PdfProfileAnalysis.fromJson(json);
 }
