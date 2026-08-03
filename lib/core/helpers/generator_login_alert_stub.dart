0230160a8dde99fea35308d8bab85e91e7267771
rashedhathalmic-crypto <rashedhathalmic@gmail.com>
2026-08-03 10:10:28 +0300

    Add non-web login alert fallback

---
+++ lib/core/helpers/generator_login_alert_stub.dart
@@ -0,0 +1,10 @@
+Future<bool> requestGeneratorNotificationPermission() async => false;
+
+void showGeneratorLoginNotification() {}
+
+Future<bool> sendGeneratorLoginEmail({
+  required String username,
+  required String accountEmail,
+}) async {
+  return false;
+}
